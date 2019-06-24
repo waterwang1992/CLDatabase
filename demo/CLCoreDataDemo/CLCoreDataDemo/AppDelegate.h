cad9f801a2dff6d49e5703c585bf35024992e103
wangcongcong <clarence@jollycorp.com>
2019-06-24 21:24:00 +0800

    初始化coreData 工程

---
+++ demo/CLCoreDataDemo/CLCoreDataDemo/AppDelegate.h
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.h
+//  CLCoreDataDemo
+//
+//  Created by 周冯婷 on 2019/3/15.
+//  Copyright © 2019年 周冯婷. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+/*
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+*/
+
+@end
+
---
+++ demo/CLCoreDataDemo/CLCoreDataDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CLCoreDataDemo
+//
+//  Created by 周冯婷 on 2019/3/15.
+//  Copyright © 2019年 周冯婷. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
